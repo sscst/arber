1b38a3d5b29a53f456e3cf4b5cd12c99f5f72acc
sscst <470840005@qq.com>
2013-02-28 11:32:16 +0800

    change

--- linkDB.py
+++ linkDB.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python  
 # -*- coding: utf-8 -*- 
-import MySQLdb,os,json
+import MySQLdb,os,json,warnings
 from utensil import *
 
 with open(os.path.join(PATH,'db.sql')) as db:
@@ -9,8 +9,10 @@ with open(os.path.join(PATH,'db.sql')) as db:
         cur = conn.cursor()
         command_lines = text.split(';')
         command_lines.pop()
-        for line in command_lines :
-            cur.execute(line + ';')
+	with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+	    for line in command_lines :
+		    cur.execute(line + ';')
         conn.commit()
         cur.close()
         conn.close()
